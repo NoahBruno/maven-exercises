73e712b37de35415ec20500c72b4e1eebd3d3a75
Noah Bruno <noah.bruno122@gmail.com>
2022-03-08 15:38:00 -0600

    iusnbfosn

---
+++ src/main/java/stringExercise.java
@@ -0,0 +1,9 @@
+import org.apache.commons.lang3.StringUtils;
+import java.util.Scanner;
+public class stringExercise {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Hello, please enter a sentence.");
+        String userInput = scanner.nextLine();
+    }
+}
